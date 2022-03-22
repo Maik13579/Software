bfafb944f2fcdec25e04f76a8d3e6ea7fff39a6f
Maik Knof <maik.knof@gmx.de>
2022-03-22 19:24:48 +0100

    add auto build to setup.bash

--- nmap/setup.bash
+++ nmap/setup.bash
@@ -1,5 +1,10 @@
 
 
 nmap(){
-	docker run --rm -it nmap "$@"
+	if [[ $(docker images) == *"nmap"* ]]; then
+		docker run --rm -it nmap "$@"
+	else # build container from Dockerfile
+		docker build -t nmap $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.
+		docker run --rm -it nmap "$@"
+	fi
 }
